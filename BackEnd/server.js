da592a7de94032c11f1368ad6091c3d793d79fe4
AbyssGazer-Oliver <oliverchen43999@gmail.com>
2022-04-30 13:07:48 -0400

    new commit

---
+++ BackEnd/server.js
@@ -0,0 +1,285 @@
+var express = require('express')
+const cors = require('cors');
+var { graphqlHTTP } = require('express-graphql')
+var { buildSchema } = require('graphql')
+
+// define GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        states(query: String): [State]
+    },
+    type State {
+        name: String
+        abbreviation: String
+    }
+`)
+
+var usStateData = [
+  {
+    name: 'Alabama',
+    abbreviation: 'AL',
+  },
+  {
+    name: 'Alaska',
+    abbreviation: 'AK',
+  },
+  {
+    name: 'American Samoa',
+    abbreviation: 'AS',
+  },
+  {
+    name: 'Arizona',
+    abbreviation: 'AZ',
+  },
+  {
+    name: 'Arkansas',
+    abbreviation: 'AR',
+  },
+  {
+    name: 'California',
+    abbreviation: 'CA',
+  },
+  {
+    name: 'Colorado',
+    abbreviation: 'CO',
+  },
+  {
+    name: 'Connecticut',
+    abbreviation: 'CT',
+  },
+  {
+    name: 'Delaware',
+    abbreviation: 'DE',
+  },
+  {
+    name: 'District Of Columbia',
+    abbreviation: 'DC',
+  },
+  {
+    name: 'Federated States Of Micronesia',
+    abbreviation: 'FM',
+  },
+  {
+    name: 'Florida',
+    abbreviation: 'FL',
+  },
+  {
+    name: 'Georgia',
+    abbreviation: 'GA',
+  },
+  {
+    name: 'Guam',
+    abbreviation: 'GU',
+  },
+  {
+    name: 'Hawaii',
+    abbreviation: 'HI',
+  },
+  {
+    name: 'Idaho',
+    abbreviation: 'ID',
+  },
+  {
+    name: 'Illinois',
+    abbreviation: 'IL',
+  },
+  {
+    name: 'Indiana',
+    abbreviation: 'IN',
+  },
+  {
+    name: 'Iowa',
+    abbreviation: 'IA',
+  },
+  {
+    name: 'Kansas',
+    abbreviation: 'KS',
+  },
+  {
+    name: 'Kentucky',
+    abbreviation: 'KY',
+  },
+  {
+    name: 'Louisiana',
+    abbreviation: 'LA',
+  },
+  {
+    name: 'Maine',
+    abbreviation: 'ME',
+  },
+  {
+    name: 'Marshall Islands',
+    abbreviation: 'MH',
+  },
+  {
+    name: 'Maryland',
+    abbreviation: 'MD',
+  },
+  {
+    name: 'Massachusetts',
+    abbreviation: 'MA',
+  },
+  {
+    name: 'Michigan',
+    abbreviation: 'MI',
+  },
+  {
+    name: 'Minnesota',
+    abbreviation: 'MN',
+  },
+  {
+    name: 'Mississippi',
+    abbreviation: 'MS',
+  },
+  {
+    name: 'Missouri',
+    abbreviation: 'MO',
+  },
+  {
+    name: 'Montana',
+    abbreviation: 'MT',
+  },
+  {
+    name: 'Nebraska',
+    abbreviation: 'NE',
+  },
+  {
+    name: 'Nevada',
+    abbreviation: 'NV',
+  },
+  {
+    name: 'New Hampshire',
+    abbreviation: 'NH',
+  },
+  {
+    name: 'New Jersey',
+    abbreviation: 'NJ',
+  },
+  {
+    name: 'New Mexico',
+    abbreviation: 'NM',
+  },
+  {
+    name: 'New York',
+    abbreviation: 'NY',
+  },
+  {
+    name: 'North Carolina',
+    abbreviation: 'NC',
+  },
+  {
+    name: 'North Dakota',
+    abbreviation: 'ND',
+  },
+  {
+    name: 'Northern Mariana Islands',
+    abbreviation: 'MP',
+  },
+  {
+    name: 'Ohio',
+    abbreviation: 'OH',
+  },
+  {
+    name: 'Oklahoma',
+    abbreviation: 'OK',
+  },
+  {
+    name: 'Oregon',
+    abbreviation: 'OR',
+  },
+  {
+    name: 'Palau',
+    abbreviation: 'PW',
+  },
+  {
+    name: 'Pennsylvania',
+    abbreviation: 'PA',
+  },
+  {
+    name: 'Puerto Rico',
+    abbreviation: 'PR',
+  },
+  {
+    name: 'Rhode Island',
+    abbreviation: 'RI',
+  },
+  {
+    name: 'South Carolina',
+    abbreviation: 'SC',
+  },
+  {
+    name: 'South Dakota',
+    abbreviation: 'SD',
+  },
+  {
+    name: 'Tennessee',
+    abbreviation: 'TN',
+  },
+  {
+    name: 'Texas',
+    abbreviation: 'TX',
+  },
+  {
+    name: 'Utah',
+    abbreviation: 'UT',
+  },
+  {
+    name: 'Vermont',
+    abbreviation: 'VT',
+  },
+  {
+    name: 'Virgin Islands',
+    abbreviation: 'VI',
+  },
+  {
+    name: 'Virginia',
+    abbreviation: 'VA',
+  },
+  {
+    name: 'Washington',
+    abbreviation: 'WA',
+  },
+  {
+    name: 'West Virginia',
+    abbreviation: 'WV',
+  },
+  {
+    name: 'Wisconsin',
+    abbreviation: 'WI',
+  },
+  {
+    name: 'Wyoming',
+    abbreviation: 'WY',
+  },
+]
+
+function escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\\s]/g, '\\$&')
+}
+
+var searchStates = function (args) {
+  if (args.query) {
+    var query = args.query
+    const regexp = new RegExp(escapeRegExp(query), 'i')
+    return usStateData.filter((state) => state.name.match(regexp))
+  } else {
+    return usStateData
+  }
+}
+var root = {
+  states: searchStates,
+}
+
+// Create an express server and a GraphQL endpoint
+var app = express()
+app.use(cors())
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+app.listen(4000)
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
---
+++ FrontEnd/map-search/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import TypeaheadMapSearch from "./components/TypeaheadMapSearch.vue";
+</script>
+
+<template>
+  <TypeaheadMapSearch
+    class="usmap-search"
+    placeholder="Please enter state to search..."
+  ></TypeaheadMapSearch>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+body {
+  background-color: #202124;
+}
+
+strong {
+  font-weight: bolder;
+}
+
+.usmap-search {
+  height: 100%;
+}
+
+html,
+body,
+#app {
+  height: 95%;
+}
+</style>
